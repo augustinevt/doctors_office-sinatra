e1d40d203de312272a7e3e86134da7202d045be0
August von Trapp and Matt Wilkin <august@>
2016-07-12 09:55:17 -0700

    passing tests for Doctor#initialize

---
+++ lib/doctor.rb
@@ -0,0 +1,28 @@
+class Doctor
+  attr_reader(:name, :id, :specialty)
+
+  def initialize(attributes)
+    @name = attributes[:name]
+    @id = attributes[:id]
+    @specialty = attributes[:specialty]
+  end
+
+  def self.all
+    returned_doctors = DB.exec("SELECT * FROM doctors")
+    doctors =[]
+    returned_doctors.each() do |doctor|
+      doctors.push(Doctor.new({name: doctor['name'], specialty: doctor['specialty'], id: doctor['id'].to_i}))
+    end
+    doctors
+  end
+
+  def save
+    result = DB.exec("INSERT INTO doctors (name, specialty) VALUES ('#{@name}', '#{@specialty}') RETURNING id;")
+    @id = result.first['id'].to_i
+  end
+
+  def ==(other_doctor)
+    self.name() == other_doctor.name() && self.id() == other_doctor.id()
+  end
+
+end
---
+++ spec/doctor_spec.rb
@@ -0,0 +1,24 @@
+require 'rspec'
+require 'pg'
+require 'doctor'
+require 'pry'
+
+DB = PG.connect({:dbname => 'doctors_office_test'})
+
+RSpec.configure do |config|
+  config.after(:each) do
+    DB.exec("DELETE FROM doctors *")
+  end
+end
+
+describe 'Doctor' do
+  describe '#initialize' do
+
+    it 'should save new doctor to database' do
+      new_doctor = Doctor.new({name: 'Doug', specialty: 'Orthopedics'})
+      new_doctor.save()
+      expect(Doctor.all()).to(eq([new_doctor]))
+    end
+
+  end
+end
